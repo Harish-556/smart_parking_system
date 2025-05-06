3dc79950db62d4e3264ef60702158e5fee8ab263
Harish-556 <harishvarma.562@gmail.com>
2025-05-06 22:19:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -203,4 +203,6 @@ def payment_confirm(booking_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensure the database and tables are created
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
